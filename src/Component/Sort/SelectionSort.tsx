998697b7d94daf3cee00bb7beff794c9f398d28d
ckskal3 <stou2@naver.com>
2020-02-28 16:31:24 +0900

    add animation ing...

--- src/Component/Sort/SelectionSort.tsx
+++ src/Component/Sort/SelectionSort.tsx
@@ -3,21 +3,43 @@ import React, { useState } from 'react';
 
 function SSort(): JSX.Element {
   const [randomArr, setRandomArr] = useState([
-    { id: 1, value: 5 },
-    { id: 2, value: 4 },
-    { id: 3, value: 2 },
-    { id: 4, value: 6 },
-    { id: 5, value: 1 },
-    { id: 6, value: 7 },
-    { id: 7, value: 8 },
-    { id: 8, value: 9 },
-    { id: 9, value: 10 },
-    { id: 10, value: 3 }]);
+    {
+      id: 1, value: 5, color: 'white', transition: '0',
+    },
+    {
+      id: 2, value: 4, color: 'white', transition: '0',
+    },
+    {
+      id: 3, value: 2, color: 'white', transition: '0',
+    },
+    {
+      id: 4, value: 6, color: 'white', transition: '0',
+    },
+    {
+      id: 5, value: 1, color: 'white', transition: '0',
+    },
+    {
+      id: 6, value: 7, color: 'white', transition: '0',
+    },
+    {
+      id: 7, value: 8, color: 'white', transition: '0',
+    },
+    {
+      id: 8, value: 9, color: 'white', transition: '0',
+    },
+    {
+      id: 9, value: 10, color: 'white', transition: '0',
+    },
+    {
+      id: 10, value: 3, color: 'white', transition: '0',
+    }]);
 
-  const delay = (): Promise<void> => new Promise((resolve) => setTimeout(resolve, 500));
+  const delay = (time: number): Promise<void> => new Promise(
+    (resolve) => setTimeout(resolve, time),
+  );
 
-  const tempDelay = async (): Promise<void> => {
-    await delay();
+  const tempDelay = async (time: number): Promise<void> => {
+    await delay(time);
   };
 
   const selectionSort = async (): Promise<void> => {
@@ -31,21 +53,50 @@ function SSort(): JSX.Element {
           min = j;
         }
       }
+      tempArr[min].color = 'green';
+      tempArr[i].color = 'green';
+
+      const minX = document.getElementById(tempArr[min].id.toString())?.getBoundingClientRect().x;
+      const iX = document.getElementById(tempArr[i].id.toString())?.getBoundingClientRect().x;
+      const dist = (minX! - iX!);
+      console.log(dist);
+      tempArr[min].transition = `${(dist * -1)}px`;
+      tempArr[i].transition = `${dist}px`;
+      setRandomArr(tempArr.slice());
+
+      await tempDelay(2000);
+
+      tempArr[i].transition = '0';
+      tempArr[min].transition = '0';
+      // setRandomArr(tempArr.slice());
+      await tempDelay(500);
+
       [tempArr[min], tempArr[i]] = [tempArr[i], tempArr[min]]; // swap
-      setRandomArr(tempArr);
+      setRandomArr(tempArr.slice());
+      await tempDelay(2000);
+
+      tempArr[min].color = 'white';
+      tempArr[i].color = 'yellow';
+      setRandomArr(tempArr.slice());
       saveStateArr = tempArr.slice();
-      await tempDelay();
+      await tempDelay(1000);
     }
   };
+
 
   const printArr = randomArr.map((item) => (
     <div
       key={item.id}
+      id={item.id.toString()}
       style={{
-        marginRight: '3px',
-        width: '20px',
+        width: '100px',
         height: `${item.value * 50}px`,
-        border: ' 1px solid black',
+        border: '1px solid black',
+        backgroundColor: `${item.color}`,
+        textAlign: 'center',
+        transform: `translateX(${item.transition})`,
+        transitionDuration: '1s',
+
       }}
     >
       {item.value}
@@ -58,7 +109,8 @@ function SSort(): JSX.Element {
         선택정렬
       </h2>
       <button type="submit" onClick={selectionSort}> 선택정렬 </button>
-      <div className="arrClass" style={{ display: 'flex' }}>
+      <br />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
         {printArr}
       </div>
     </div>
