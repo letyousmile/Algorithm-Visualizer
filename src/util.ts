51740993972a88129e1e9e5cfbe707e7a0c206f8
sunghyun <sunghyunyook@gmail.com>
2020-03-04 10:41:27 +0900

    graph init

--- src/util.ts
+++ src/util.ts
@@ -6,8 +6,19 @@ export type GraphBar = {
   sorted: boolean;
   height: number;
 };
+export type Node = {
+  key: number;
+  connted: number[];
+  color: string;
+  isVisited: boolean;
+};
 export type Process = {
   arr: number[];
+  targets: number[];
+  phase: string;
+};
+export type GProcess = {
+  visited: boolean[];
   targets: number[];
   phase: string;
 };
