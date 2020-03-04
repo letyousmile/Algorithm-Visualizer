555ff125a347e5b94217f4bac545bb24c9fc4f0b
KimByungChul <bcking92@gmail.com>
2020-03-04 17:59:39 +0900

    made line with svg

--- src/Component/Graph/GraphNodes.tsx
+++ src/Component/Graph/GraphNodes.tsx
@@ -11,7 +11,7 @@ function GraphNodes({ graphNodes, graphLines }: any): JSX.Element {
         height: '50px',
         width: '50px',
         backgroundColor: node.color,
-        transform: `translate(${400 + 100 * Math.floor(node.key / 2)}px, ${200 + ((-1) ** Math.ceil(node.key / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(node.key / 2) - 200) ** 2)) / 1.5)}px)`,
+        transform: `translate(${400 + 100 * Math.floor(node.key / 2)}px, ${200 + ((-1) ** Math.ceil(node.key / 2)) * 50 + ((-1) ** Math.ceil(node.key / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(node.key / 2) - 200) ** 2)) / 1.5)}px)`,
         zIndex: 1,
       }}
       key={node.key}
@@ -27,23 +27,23 @@ function GraphNodes({ graphNodes, graphLines }: any): JSX.Element {
     </div>
   ));
   const lines = graphLines.map((line: Line) => (
-    <div
+    <line
+      x1={`${425 + 100 * Math.floor(line.from / 2)}`}
+      y1={`${225 + ((-1) ** Math.ceil(line.from / 2)) * 50 + ((-1) ** Math.ceil(line.from / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(line.from / 2) - 200) ** 2)) / 1.5)}`}
+      x2={`${425 + 100 * Math.floor(line.to / 2)}`}
+      y2={`${225 + ((-1) ** Math.ceil(line.to / 2)) * 50 + ((-1) ** Math.ceil(line.to / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(line.to / 2) - 200) ** 2)) / 1.5)}`}
       style={{
-        position: 'absolute',
-        display: 'block',
-        height: '0px',
-        padding: '3px 0',
-        width: '100px',
-        backgroundColor: line.color,
-        transform: `translate(${400 + 100 * Math.floor(line.from / 2) + 25}px, ${200 + ((-1) ** Math.ceil(line.from / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(line.from / 2) - 200) ** 2)) / 1.5) + 25}px)`,
+        stroke: line.color,
+        strokeWidth: 2,
       }}
-      key={line.key}
     />
   ));
   return (
     <div>
       {nodes}
-      {lines}
+      <svg height="1000" width="2000">
+        {lines}
+      </svg>
     </div>
   );
 }
