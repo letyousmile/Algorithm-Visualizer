1322af3365d6076f0d24ea9f6d9d453f490feaa0
KimByungChul <bcking92@gmail.com>
2020-03-06 10:51:10 +0900

    fix dfs

--- src/Component/Graph/GraphAlgorithm.ts
+++ src/Component/Graph/GraphAlgorithm.ts
@@ -64,6 +64,89 @@ export function dfs(nodeList: Node[]): GProcess[] {
   const visitedLine: string[] = [];
   let targetLine = '';
   const stack: number[][] = [];
+  process.push({
+    visitedNode: visitedNode.slice(),
+    visitedLine: visitedLine.slice(),
+    targetNodes: [],
+    targetLine,
+    phase: 'start',
+    list: stack.map((el) => el[1]),
+  });
+  stack.push([0, 0]);
+  visitedNode.push(0);
+  process.push({
+    visitedNode: visitedNode.slice(),
+    visitedLine: visitedLine.slice(),
+    targetNodes: [],
+    targetLine,
+    phase: 'visit',
+    list: stack.map((el) => el[1]),
+  });
+  let now = 0;
+  while (stack.length > 0) {
+    let flag = true;
+    const hereNode = nodeList[now];
+    for (let i = 0; i < hereNode.connected.length; i += 1) {
+      if (!visitedNode.includes(hereNode.connected[i])) {
+        targetLine = now < hereNode.connected[i] ? `${now}to${hereNode.connected[i]}` : `${hereNode.connected[i]}to${now}`;
+        process.push({
+          visitedNode: visitedNode.slice(),
+          visitedLine: visitedLine.slice(),
+          targetNodes: [now, hereNode.connected[i]],
+          targetLine,
+          phase: 'visit',
+          list: stack.map((el) => el[1]),
+        });
+        targetLine = '';
+        stack.push([now, hereNode.connected[i]]);
+        visitedNode.push(hereNode.connected[i]);
+        visitedLine.push(targetLine);
+        now = hereNode.connected[i];
+        process.push({
+          visitedNode: visitedNode.slice(),
+          visitedLine: visitedLine.slice(),
+          targetNodes: [now, now],
+          targetLine,
+          phase: 'afterVisit',
+          list: stack.map((el) => el[1]),
+        });
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      const p = stack.pop();
+      if (p !== undefined) {
+        const temp = p[0];
+        now = temp;
+        process.push({
+          visitedNode: visitedNode.slice(),
+          visitedLine: visitedLine.slice(),
+          targetNodes: [now, now],
+          targetLine,
+          phase: 'pop',
+          list: stack.map((el) => el[1]),
+        });
+      }
+    }
+  }
+  process.push({
+    visitedNode: visitedNode.slice(),
+    visitedLine: visitedLine.slice(),
+    targetNodes: [now, now],
+    targetLine,
+    phase: 'done',
+    list: stack.map((el) => el[1]),
+  });
+  return process;
+}
+
+export function dfsR(nodeList: Node[]): GProcess[] {
+  const process: GProcess[] = [];
+  const visitedNode: number[] = [0];
+  const visitedLine: string[] = [];
+  let targetLine = '';
+  const stack: number[][] = [];
   stack.push([0, 0]);
   process.push({
     visitedNode: visitedNode.slice(),
@@ -115,8 +198,7 @@ export function dfs(nodeList: Node[]): GProcess[] {
       });
       const hereNode = nodeList[nowTo];
       for (let j = 0; j < hereNode.connected.length; j += 1) {
-        if (!visitedNode.includes(hereNode.connected[j])
-          && !stack.map((el) => el[1]).includes(hereNode.connected[j])) {
+        if (!visitedNode.includes(hereNode.connected[j])) {
           stack.push([nowTo, hereNode.connected[j]]);
           process.push({
             visitedNode: visitedNode.slice(),
--- src/Component/Graph/GraphNodes.tsx
+++ src/Component/Graph/GraphNodes.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Node, Line } from '../../util';
 
+function positionX(key: number): number {
+  return (400 + 100 * Math.floor(key / 2));
+}
+function positionY(key: number): number {
+  return (200 + ((-1) ** Math.ceil(key / 2)) * 50 + ((-1) ** Math.ceil(key / 2))
+  * (Math.sqrt(40000 - ((100 * Math.floor(key / 2) - 200) ** 2)) / 1.5));
+}
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function GraphNodes({ graphNodes, graphLines, list }: any): JSX.Element {
   const nodes = graphNodes.map((node: Node) => (
@@ -12,7 +19,7 @@ function GraphNodes({ graphNodes, graphLines, list }: any): JSX.Element {
         height: '50px',
         width: '50px',
         backgroundColor: node.color,
-        transform: `translate(${400 + 100 * Math.floor(node.key / 2)}px, ${200 + ((-1) ** Math.ceil(node.key / 2)) * 50 + ((-1) ** Math.ceil(node.key / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(node.key / 2) - 200) ** 2)) / 1.5)}px)`,
+        transform: `translate(${positionX(node.key)}px, ${positionY(node.key)}px)`,
         zIndex: 100,
       }}
       key={node.key}
@@ -29,10 +36,10 @@ function GraphNodes({ graphNodes, graphLines, list }: any): JSX.Element {
   ));
   const lines = graphLines.map((line: Line) => (
     <line
-      x1={`${425 + 100 * Math.floor(line.from / 2)}`}
-      y1={`${225 + ((-1) ** Math.ceil(line.from / 2)) * 50 + ((-1) ** Math.ceil(line.from / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(line.from / 2) - 200) ** 2)) / 1.5)}`}
-      x2={`${425 + 100 * Math.floor(line.to / 2)}`}
-      y2={`${225 + ((-1) ** Math.ceil(line.to / 2)) * 50 + ((-1) ** Math.ceil(line.to / 2)) * (Math.sqrt(40000 - ((100 * Math.floor(line.to / 2) - 200) ** 2)) / 1.5)}`}
+      x1={`${positionX(line.from) + 25}`}
+      y1={`${positionY(line.from) + 25}`}
+      x2={`${positionX(line.to) + 25}`}
+      y2={`${positionY(line.to) + 25}`}
       style={{
         stroke: line.color,
         strokeWidth: 2,
@@ -68,15 +75,20 @@ function GraphNodes({ graphNodes, graphLines, list }: any): JSX.Element {
       <div style={{
         display: 'flex',
         height: '400px',
+        width: '70px',
+        borderLeft: '2px solid black',
+        borderRight: '2px solid black',
+        borderBottom: '2px solid black',
         position: 'absolute',
-        transform: 'translate(1000px, 100px)',
+        transform: 'translate(1000px, 30px)',
         flexDirection: 'column-reverse',
         justifyContent: 'flex-start',
+        alignItems: 'center',
       }}
       >
         {stackOrQueue}
       </div>
-      <svg height="1000" width="2000">
+      <svg height="900" width="1600">
         {lines}
       </svg>
     </div>
